949a58dcca470e2235afe92c8ad3c9d8cc903cf2
Сергей Иванов <sergejivanov@Mac-mini-Sergej.local>
2019-01-17 13:40:43 +0300

    Initial Commit

---
+++ PersonalyQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalyQuiz
+//
+//  Created by Сергей Иванов on 17/01/2019.
+//  Copyright © 2019 topMob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
